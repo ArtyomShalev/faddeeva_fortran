8f74594643d7d56b15ab7bfd2130262babe55a68
ArtyomShalev <artyomshalev@gmail.com>
2022-01-18 15:58:44 +0300

    rename vars and module

--- errfun_MIT/src/lib/faddeeva_fortran_interface.f90
+++ errfun_MIT/src/lib/faddeeva_fortran_interface.f90
@@ -1,23 +1,23 @@
 module faddeeva_fortran_interface
 !    use iso_c_binding
     interface
-        complex (c_double_complex) function faddeeva_w(z, relerr) bind(c, name = 'Faddeeva_w')
+        complex (c_double_complex) function Faddeeva_w(z, relerr) bind(c, name = 'Faddeeva_w')
             use iso_c_binding
             complex(c_double_complex), value :: z
             real(c_double), value :: relerr
-        end function faddeeva_w
+        end function Faddeeva_w
 
-        complex (c_double_complex) function faddeeva_erf(z, relerr) bind(c, name = 'Faddeeva_erf')
+        complex (c_double_complex) function Faddeeva_erf(z, relerr) bind(c, name = 'Faddeeva_erf')
             use iso_c_binding
             complex(c_double_complex), value :: z
             real(c_double), value :: relerr
-        end function faddeeva_erf
+        end function Faddeeva_erf
 
-        complex (c_double_complex) function faddeeva_erfcx(z, relerr) bind(c, name = 'Faddeeva_erfcx')
+        complex (c_double_complex) function Faddeeva_erfcx(z, relerr) bind(c, name = 'Faddeeva_erfcx')
             use iso_c_binding
             complex(c_double_complex), value :: z
             real(c_double), value :: relerr
-        end function faddeeva_erfcx
+        end function Faddeeva_erfcx
 
 !        erfc, erfi, Dawson
     end interface
