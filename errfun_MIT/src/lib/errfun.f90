4f9ff471ffbbaeaf614cc2a9c3323e1ec791f3cd
ArtyomShalev <artyomshalev@gmail.com>
2022-01-18 13:29:04 +0300

    init commit

--- errfun_MIT/src/lib/errfun.f90
+++ errfun_MIT/src/lib/errfun.f90
@@ -1,6 +1,5 @@
 module faddeeva_fortran_interface
 !    use iso_c_binding
-
     interface
         complex (c_double_complex) function faddeeva_w(z, relerr) bind(c, name = 'Faddeeva_w')
             use iso_c_binding
