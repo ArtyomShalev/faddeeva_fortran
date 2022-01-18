83a04ae79b6b43facd0eeb3f463aaeedaedcd260
ArtyomShalev <artyomshalev@gmail.com>
2022-01-18 18:00:20 +0300

    documentation added

--- main.f90
+++ main.f90
@@ -1,32 +1,25 @@
 program main
     use faddeeva_fortran_interface
     integer, parameter :: dp=kind(0.d0)
-    complex(dp) :: z_arg, z_res_lib, z_res_multem2
-    complex(dp), parameter ::                                                   &
-            cone = cmplx( 1.0_dp, 0.0_dp, kind=dp ),                            &
-            ci = cmplx( 0.0_dp, 1.0_dp, kind=dp ),                              &
-            czero = cmplx( 0.0_dp, 0.0_dp, kind=dp )
+!    complex(dp), allocatable :: z(:)
+    complex(dp) :: z
+!    complex(dp), allocatable :: result(:)
     complex(dp) :: result
     real(dp) :: relerr
-!    real(dp), parameter:: emach = 1.0d-8, step = 1.0_dp
-!    integer i, j
-!    integer, parameter:: total =100
-!    real(dp), parameter:: start = -step*total, stop = -start
-!    real(dp) x,y, z_abs
     !=======================================================================
-
-    write(*,*) 'w'
-    result = Faddeeva_w(cone, relerr)
+    !FADDEEVA FUNCTION
+    !Usage: result = Faddeeva_w(z, relerr)
+    !Compute: w(z) = exp(-z*z)*erfc(-i*z)
+    !z:  a complex value (not an array)
+!    allocate(z(1))
+!    allocate(result(1))
+!    z = ( cmplx(1.0_dp, 0.0_dp, kind=dp), cmplx(5.0_dp, 0.0_dp, kind=dp) )
+    !TODO relerr and cmplx kind dont effect the result
+    z = cmplx(1.0_dp, 15.0_dp, kind=dp)
+!    z = (/ cmplx(1.0_dp, 15.0_dp) /)
+!    z(1) = cmplx(1.0_dp, 15.0_dp)
+    relerr = 1.0e-14_dp
+    result = Faddeeva_w(z, relerr)
     write(*,*) result
-    write(*,*) '-------------------------------------------------------------'
 
-    write(*,*) 'erf'
-    result = Faddeeva_erf(cone, relerr)
-    write(*,*) result
-    write(*,*) '-------------------------------------------------------------'
-
-    write(*,*) 'erfcx'
-    res = Faddeeva_erfcx(cone, relerr)
-    write(*,*) result
-    write(*,*) '-------------------------------------------------------------'
 end program
